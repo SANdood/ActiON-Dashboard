5ba8146ec638034819795c91d494bf1f252a331a
625alex <625alex@gmail.com>
2015-01-16 14:04:28 -0700

    update to battery icon

--- app-v4/script4.4.js
+++ app-v4/script4.4.js
@@ -140,7 +140,7 @@ function setIcons() {
 	$(".temperature").append("<div class='footer'><i class='fa fa-fw wi wi-thermometer'></i></div>");
 	$(".energy").append("<div class='footer'><i class='fa fa-fw wi wi-lightning'></i></div>");
 	$(".power").append("<div class='footer'><i class='fa fa-fw fa-bolt'></i></div>");
-	$(".battery").append("<div class='footer'><span class='batt'></span></div>");
+	$(".battery").append("<div class='footer'><i class='fa fa-fw batt'></i></div>");
 	
 	$(".tile[data-is-value=true]").each(function(){renderValue($(this))});
 }
